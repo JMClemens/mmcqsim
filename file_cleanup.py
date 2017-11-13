b203fa88517f4ef296dd1c2a489539041722142b
JMClemens <joshuamclemens@gmail.com>
2017-11-13 15:04:42 -0500

    adding comments, fixing formatting

--- file_cleanup.py
+++ file_cleanup.py
@@ -1,5 +1,7 @@
 import os
 
+# This function makes a list of all '.txt' files 
+# in our temp folder and removes them
 def cleanupTxtFiles():
     mydir = os.getcwd() + '/temp'
     filelist = [f for f in os.listdir(mydir) if f.endswith(".txt")]
@@ -7,6 +9,8 @@ def cleanupTxtFiles():
         os.remove(os.path.join(mydir,f))
     print("Txt files deleted.")
 
+# This function makes a list of all '.csv' files 
+# in our temp folder and removes them
 def cleanupCsvFiles():
     mydir = os.getcwd() + '/temp'
     filelist = [f for f in os.listdir(mydir) if f.endswith(".csv")]
@@ -14,14 +18,16 @@ def cleanupCsvFiles():
         os.remove(os.path.join(mydir,f))
     print("CSV files deleted.")
 
+# The function allows the user to choose which
+# data they want to keep and what they would like to delete
 def cleanupPrompt():
-    option1 = raw_input('Would you like to delete TXT files with average queue wait time \n'
-    + 'and length statistical data from this simulation? (y/n)').lower()
+    option1 = raw_input("Would you like to delete TXT files with average queue wait time " +
+         "\n and length statistical data from this simulation? (y/n)\n").lower()
     if(option1 == 'y'):
         cleanupTxtFiles()
     
-    option2 = raw_input("Would you like to delete CSV files with average queue wait time \n"
-    	+ "and length from this simulation? (y/n)").lower()
+    option2 = raw_input("Would you like to delete CSV files with average queue wait time " +
+        "\n and length from this simulation? (y/n)\n").lower()
     if(option2 == 'y'):
         cleanupCsvFiles()
 
